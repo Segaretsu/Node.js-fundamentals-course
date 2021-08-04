37e71208414f584816abfe9e096ea43bde9eb006
Jhon Sebastian <Sebastamriel@gmail.com>
2021-08-03 22:00:55 -0500

    basic http - 03/08/2021, 10:00p.m.

---
+++ modulos/http.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+http.createServer(router).listen(process.env.PORT || 3000);
+
+function router(request, response) {
+    console.log('New request!');
+    console.log(request.url);
+
+    switch (request.url) {
+        case '/hello':
+            response.write('Hello there, How are you?');
+            response.end();
+            break;
+        default:
+            response.write('Error 404: I have no idea of your need');
+            response.end();
+    }
+    // response.writeHead(201, { 'Content-type': 'text/plain' });
+    // response.write('Hello!!, I already know how to use HTTP in Nodejs');
+    // response.end();
+}
+
+console.log('Listen https in port: ' + (process.env.PORT || 3000))
